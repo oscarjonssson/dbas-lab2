3b10962d9c8a77b07fd6dd7b928daba2d267d945
oscarjonssson <117173831+oscarjonssson@users.noreply.github.com>
2023-12-17 19:45:14 +0100

    working on 2c

--- connect.py
+++ connect.py
@@ -54,82 +54,18 @@ def get_available_physical_books():
     result = cur.fetchall()
     print(result)
 
-#function to check if the user is a student
-def is_student(user_id):
-    cur.execute("SELECT * FROM students WHERE userid = %s", (user_id,))
-    result = cur.fetchone()
-    if len(result) > 0:
-        return True
-    else:
-        return False
 
-# function to check if the user has any fines
-
-def check_fine(cur, user_id):
-    cur.execute("SELECT * FROM fines WHERE userid = %s", (user_id,))
-    result = cur.fetchone()
-    if result:
-        print("You have a fine")
-        return False
-    else:
-        return True
-    
-def check_nr_of_borrowed_books(cur, user_id):
-    cur.execute("SELECT COUNT(*) FROM borrowing WHERE userid = %s", (user_id,))
-    result = cur.fetchone()
-    if result[0] >= 5:
-        print("You have borrowed 5 books")
-        return False
-    else:
-        return True
-    
-def check_isbn(cur, user_id, isbn):
-    cur.execute("SELECT * FROM books WHERE isbn = %s", (isbn,))
-    result = cur.fetchone()
-    if result:
-        return True
-    else:
-        print("Invalid ISBN")
-        return False
-    
-def retrieve_values_for_insert(user_id, isbn):
-    cur.execute("SELECT bookid FROM books WHERE isbn = %s", (isbn,))
-    book_id = cur.fetchone()
-    cur.execute("SELECT physicalid FROM resources WHERE bookid = %s", (book_id,))
-    physical_id = cur.fetchone()
-    cur.execute("INSERT INTO borrowing (userid, physicalid) VALUES (%s, %s)", (user_id, physical_id))
-    conn.commit()
-    print("You have borrowed the book")
 
 # function to borrow a book using a given email of an existing user in the database.
 # - Check that the email exists in the database.
 # - Add the borrowed book in the relevant table and tell the user when they’re expected to return it.
 def borrow_book():
-    # Check if the email exists in the users table
-    email = input("Please enter email: ")
-    cur.execute("SELECT userID FROM users WHERE email = %s", (email,))
-    user_id = cur.fetchone()
-
-    if user_id:
-        # Check constraints
-        # For students
-        if is_student(user_id):
-            if check_fine(cur, user_id) and check_nr_of_borrowed_books(cur, user_id):
-                isbn = input("Please enter the ISBN of the book you want to borrow: ")
-                if check_isbn(cur, user_id, isbn):
-                    retrieve_values_for_insert(user_id, isbn)
-
-        # For Admins
-        else:
-            isbn = input("Please enter the ISBN of the book you want to borrow: ")
-            retrieve_values_for_insert(user_id, isbn)
-    else:
-        print("Invalid Email")
+   
 
 if __name__ == "__main__":
+    borrow_book()
     get_available_physical_books()
     get_book_title_by_genre()
     get_physical_books_by_title()
-    borrow_book()
     
     
